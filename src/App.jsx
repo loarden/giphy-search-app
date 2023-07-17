6119e042ec56ff9f20ba0cd6da390e85c3100ccc
loarden <biacsi.ors@gmail.com>
2023-07-17 18:46:28 +0200

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useContext } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 import { GiphyFetch } from "@giphy/js-fetch-api";
-import { Gif, Grid, SearchBar } from "@giphy/react-components";
-
-const giphyFetch = new GiphyFetch("wphvpzgvQnbIL94cwQbjFrb16u4pNlg0");
+import {
+  Gif,
+  Grid,
+  SearchBar,
+  SuggestionBar,
+  SearchContext,
+} from "@giphy/react-components";
 
 function App() {
-  const [gif, setGif] = useState("f");
+  const { fetchGifs, searchKey } = useContext(SearchContext);
 
-  const searchGiphy = useCallback((offset) => giphyFetch.search(gif, { offset, limit: 20 }),[gif]);
-
-  const trendighGiphy = (offset) =>
-    giphyFetch.trending(gif, { offset, limit: 20 });
+  useEffect(() => {
+    console.log(searchKey)
+  },[searchKey])
 
   return (
     <>
-    <input type="text" value={gif} onChange={(e) => setGif(e.target.value)} />
-    {
-      gif ? <Grid key={gif} fetchGifs={searchGiphy} columns={6} width={800}/> : <Grid fetchGifs={trendighGiphy} columns={6} width={800} />
-    }
+      <SearchBar />
+      <SuggestionBar />
+      {<Grid key={searchKey} fetchGifs={fetchGifs} columns={4} width={1280} />}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { SearchContextManager } from "@giphy/react-components";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <SearchContextManager apiKey="wphvpzgvQnbIL94cwQbjFrb16u4pNlg0">
     <App />
-  </React.StrictMode>,
-)
+    </SearchContextManager>
+  </React.StrictMode>
+);
